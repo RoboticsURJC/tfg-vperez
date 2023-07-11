a4241176f889b6e2093f2741b686a927fcbc8408
vidal <v.perezb.2019@alumnos.urjc.es>
2023-07-11 11:09:51 +0200

    packages

---
+++ src/software/g_arm_moveit/launch/spawn_controllers.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_spawn_controllers_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("g_arm", package_name="g_arm_moveit").to_moveit_configs()
+    return generate_spawn_controllers_launch(moveit_config)
